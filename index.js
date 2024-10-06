c88ec7501cf47372cfcd656959153f7258d813f2
SAMRAT69 <157216723+SAMRAT69@users.noreply.github.com>
2024-10-06 16:50:16 +0545

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/delta/bypass", async (req, res) => {
         );
         if (DeltaAuthResponse.data.Result) {
           result = DeltaAuthResponse.data.Result;
-          console.log("Success:", key);
+          console.log("Success:", result);
         } else {
           return res.status(500).json({
             error: "Failed to bypass url",
